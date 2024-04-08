5dd8fee180b5b84ae5d272c6ffe0cf3050b805c6
drodriguez97 <danrod.biz@gmail.com>
2024-04-07 20:19:21 -0600

    Initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <header className="bg-slate-700 p-8 drop-shadow-2xl py-9">
+                <h1 className="text-white text-5xl font-bold font-roboto text-left">Cine<span className="text-blue-400">Match</span></h1>
+        </header>
+    )
+  }
+  
+  export default Header
